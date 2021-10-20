6e5a6f4884b64c7ba0e434326211833865ae9322
Bojdan <91680030+Bojdan@users.noreply.github.com>
2021-10-20 13:51:15 +0300

    Serching

--- my_film.py
+++ my_film.py
@@ -1,16 +1,56 @@
-from films1 import Film
+from films1 import Film, Actor
+import datetime
 
 
-x = Film("Kill Bill 3", "Драмма", True, {"Нью-Йорк": 2004, "Москва": 2005, "Токио": 2006}, ["Jhoni", "Bill", "Uma"], "Tarantino", 99, 8000000, 10000000, ["Rus", "Eng"])
+def connekt_kino(film, *actors):
+    """
+    Связь актёра и фильма
+    """
+    for actor in actors:
+        film.actors.append(actor)
+        actor.films.append(film)
+    film.show_actors()
 
 
 
-x.info()
-x.show_actors()
+x = Film("Kill Bill 3", "Драмма", True, {"Нью-Йорк": 2004, "Москва": 2005, "Токио": 2006},
+         "Tarantino", 99, 8000000, 10000000, ["Rus", "Eng"], "Нормальный фильм, идите и посмотрите!"
+         )
 
-print("\n" + "Бюджет филма составляет:", x.budget, "$$$")
-print("\n" + "Сборы по миру: ", x.box_office, "$$$")
+x2 = Film("Властелин колец", "Фентези", True, {"Аризона": 2003},
+          "Питер Джексон", 180, 2000000, 30000000, ["Rus", "Eng"], "топ кинцо"
+          )
 
-print(x.leng_translate("Eng"))
+x3 = Film("Мишка фредди", "Мультмк", False, {"Аргентина": 2012},
+          "Франк Берни", 131, 180000, 22000000, ["Rus", "Eng"], "Ужс"
+          )
 
-x.date_releas("Москва")
+
+film_list = [x, x2, x3]
+
+def yers_serch(film_list):
+    for m in film_list:
+        if not m.mature:
+            print(m)
+
+def raiting_serch(min_rating, max_rating, actor_list):
+    for a in actor_list:
+        if a.rating >= min_rating and a.rating <= max_rating:
+            print(a)
+
+
+actor1 = Actor("Джони", "Дэпп", datetime.date(1988, 5, 1),
+               4.7, "крутой чел", "фото нет")
+actor2 = Actor("Роберт", "Дауни", datetime.date(1987, 4, 13), 4.0, "Сложный", "Фото нет")
+actor3 = Actor("Том", "Харди", datetime.date(1876, 5, 4), 4.3, "Сложный", "Фото нет")
+
+
+
+actor_list = [actor3, actor2, actor1]
+
+
+# connekt_kino(x, actor1, actor2, actor3)
+
+raiting_serch(4.5, 5, actor_list)
+
+yers_serch(film_list)
